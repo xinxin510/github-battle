4a8ab7d6c7604befad78623db1b2fcd7673bd8c2
Xinxin Li <xinxinli510@gmail.com>
2022-05-29 16:51:11 -0500

    allow users to select two githubs to battle

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -2,13 +2,15 @@ import React from 'react';
 import Instruction from './Instruction.js';
 import PlayerInput from './PlayerInput.js';
 import PlayerPreview from './PlayerPreview.js';
+import Result from './Result.js';
 
 export default class Battle extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       playerOne: null,
-      playerTwo: null
+      playerTwo: null,
+      battle: false
     }
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleReset = this.handleReset.bind(this);
@@ -27,7 +29,12 @@ export default class Battle extends React.Component {
   }
 
   render() {
-    const { playerOne, playerTwo } = this.state;
+    const { playerOne, playerTwo, battle } = this.state;
+
+    if (battle) {
+      return <Result />
+    }
+
     return (
       <>
         <Instruction />
@@ -58,6 +65,14 @@ export default class Battle extends React.Component {
               />
             }
           </div>
+          {playerOne && playerTwo && (
+            <button
+              className='btn dark-btn btn-space'
+              onClick={() => this.setState({battle: true})}
+            >
+              Battle
+            </button>
+          )}
         </div>
       </>
     )
