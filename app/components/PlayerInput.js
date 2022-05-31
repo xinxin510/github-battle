3e32b622b29486cf18746938f9948ca202e68144
Xinxin Li <xinxinli510@gmail.com>
2022-05-30 19:37:19 -0500

    allow users to toggle light and dark theme

--- app/components/PlayerInput.js
+++ app/components/PlayerInput.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ThemeConsumer } from '../contexts/theme.js';
 
 export default class PlayerInput extends React.Component {
   constructor(props) {
@@ -25,29 +26,33 @@ export default class PlayerInput extends React.Component {
 
   render() {
     return (
-      <form className='column player' onSubmit={this.handleSubmit}>
-        <label htmlFor='username' className='player-label'>
-          {this.props.label}
-        </label>
-        <div className='row player-inputs'>
-          <input
-            type='text'
-            id='username'
-            className='input-light'
-            placeholder='github username'
-            autoComplete='off'
-            value={this.state.username}
-            onChange={this.handleChange}
-          />
-          <button
-            className='btn dark-btn'
-            type='submit'
-            disabled={!this.state.username}
-          >
-            Submit
-          </button>
-        </div>
-      </form>
+      <ThemeConsumer>
+        {({theme}) => (
+          <form className='column player' onSubmit={this.handleSubmit}>
+            <label htmlFor='username' className='player-label'>
+              {this.props.label}
+            </label>
+            <div className='row player-inputs'>
+              <input
+                type='text'
+                id='username'
+                className={`input-${theme}`}
+                placeholder='github username'
+                autoComplete='off'
+                value={this.state.username}
+                onChange={this.handleChange}
+              />
+              <button
+                className={`btn ${theme === 'dark' ? 'light-btn' : 'dark-btn'}`}
+                type='submit'
+                disabled={!this.state.username}
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+        )}
+      </ThemeConsumer>
     )
   }
 }
--- app/index.js
+++ app/index.js
@@ -3,13 +3,37 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Popular from './components/Popular.js';
 import Battle from './components/Battle.js';
+import {ThemeProvider} from './contexts/theme.js';
+import Nav from './components/Nav.js';
+import {Routes, Route, BrowserRouter as Router} from 'react-router-dom';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      theme: 'light',
+      toggleTheme: () => {
+        this.setState(({theme}) => ({
+          theme: theme === 'light' ? 'dark' : 'light'
+        }))
+      }
+    }
+  }
   render() {
     return (
-      <div className='container'>
-        <Popular />
-      </div>
+      <Router>
+        <ThemeProvider value={this.state}>
+          <div className={this.state.theme}>
+            <div className='container'>
+              <Nav />
+              <Routes>
+                <Route exact path='/' element={<Popular />} />
+                <Route path='/battle' element={<Battle />} />
+              </Routes>
+            </div>
+          </div>
+        </ThemeProvider>
+      </Router>
     )
   }
 }
